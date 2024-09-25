0f1bb9eeb9173d6d627b221d7996ddf0137f0a78
samalsubrat <samalsubrat919@gmail.com>
2024-09-25 19:33:00 +0530

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 import { AnimatedShinyTextDemo } from "@/components/Button";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-custom-gradient pt-2 pb-20 sm:py-20">
-        <MaxWidthWrapper className="space-y-4">
+      <div className="bg-custom-gradient -mt-5 pb-20 sm:py-28 relative overflow-clip">
+        <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[3000px] lg:h-[968px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border-[#2A73B3] bg-[radial-gradient(closest-side,#0a0a0a_82%,#2A73B3)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
+        <MaxWidthWrapper className="space-y-4 relative flex flex-col justify-center items-center">
           <div className="space-y-1 relative flex flex-col justify-center items-center">
-          <video
-                src="/ai.webm"
-                autoPlay
-                muted
-                width="400"
-                height="400"
-                className="block sm:hidden"
-              />
-          <AnimatedShinyTextDemo/>
+            <video
+              src="/ai.webm"
+              autoPlay
+              muted
+              width="400"
+              height="400"
+              className="block sm:hidden"
+            />
+            <AnimatedShinyTextDemo />
             <div className="flex items-end relative">
-              <h1 className="text-7xl font-bold tracking-tight text-center">
-                Hi, I&apos;m Subrat,
+              <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold tracking-tight text-center">
+                Hi, I&apos;m Subrat, a
               </h1>
               <video
                 src="/ai.webm"
@@ -29,11 +31,11 @@ export default function Home() {
                 className="absolute -right-1/2 hidden sm:block"
               />
             </div>
-            <h1 className="text-7xl font-bold tracking-tight text-center text-[#85d2ff]">
-              a software engineer
+            <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold tracking-tight text-center text-[#85d2ff]">
+              designer & developer
             </h1>
             <div className="flex items-end relative">
-              <h1 className="text-7xl font-bold tracking-tight text-center">
+              <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold tracking-tight text-center">
                 based in India
               </h1>
               <video
@@ -42,17 +44,17 @@ export default function Home() {
                 muted
                 width="400"
                 height="400"
-                className="absolute -bottom-3/4 -left-[69%] rotate-180 hidden sm:block"
+                className="absolute -bottom-3/4 -left-[73%] rotate-180 hidden sm:block"
               />
             </div>
           </div>
-          <p className="text-center text-gray-300 font-medium text-lg">
-            I blend technical expertise with design to create efficient,
-            scalable applications
-            <br />
-            that deliver seamless and engaging user experiences.
+          <p className="text-center text-gray-300 font-medium text-base md:text-lg">
+            I blend technical expertise with design to<br />create efficient,
+            scalable applications that deliver<br />seamless and engaging user experiences.
           </p>
+          <Button variant="secondary">Show my work</Button>
         </MaxWidthWrapper>
+      
         {/* <div className="bg-black rounded-full h-[700px] w-[700px] absolute bottom-0 left-1/2 translate-x-[-50%]"/> */}
       </div>
     </>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,3 @@
-import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 import { cn } from "@/lib/utils";
 import AnimatedShinyText from "@/components/magicui/animated-shiny-text";
@@ -8,12 +7,11 @@ export async function AnimatedShinyTextDemo() {
     <div className="z-10 flex pb-2 sm:py-4 items-center justify-center">
       <div
         className={cn(
-          "group rounded-full border  transition-all ease-in hover:cursor-pointer border-white/5 bg-[#11283d] hover:bg-[#0a1724]",
+          "group transition-all ease-in",
         )}
       >
-        <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:duration-300 hover:text-neutral-400">
-          <span>✨ Contact me</span>
-          <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+        <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out  hover:duration-300 hover:text-neutral-300">
+          <span className="tracking-wider">Scripting stories through code✨</span>
         </AnimatedShinyText>
       </div>
     </div>
